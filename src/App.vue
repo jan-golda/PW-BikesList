2545c3bb3778386029f958b62e2e2e599fe4e003
Jan Golda <jan.golda@gmail.com>
2021-10-01 03:32:41 +0200

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <v-app>
     <v-main>
-      <v-container style="max-width: 1000px">
+      <v-container class="my-10" style="max-width: 1000px">
         <BikesList />
       </v-container>
     </v-main>
+    <v-footer dark padless>
+      <v-col class="text-center">
+        Jan Gołda | Project 2 | Advanced web applications | Warsaw University of Technology | 2021
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
