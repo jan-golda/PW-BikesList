19d8d50db6819c2ae93cf1e607f4e09cfb7eb0cd
Jan Golda <jan.golda@gmail.com>
2021-10-06 02:15:31 +0200

    Improved esthetics

--- src/components/Bike.vue
+++ src/components/Bike.vue
@@ -1,36 +1,37 @@
 <template>
   <v-card class="bike-card">
-    <div class="d-flex flex-no-wrap justify-space-between pa-3">
-      <div>
-        <v-card-title>
-          {{ name }}
-          <v-chip>{{ price }} EUR</v-chip>
-        </v-card-title>
-        <v-card-subtitle> </v-card-subtitle>
-        <v-card-text v-text="description"></v-card-text>
-        <div class="pa-4 pt-0">
+    <div class="pa-3">
+      <div class="d-flex flex-nowrap justify-space-between">
+        <div>
+          <v-card-title>
+            {{ name }}
+            <v-chip outlined small class="ml-4">{{ price }} EUR</v-chip>
+          </v-card-title>
+          <v-card-text v-text="description"></v-card-text>
+        </div>
+        <v-avatar tile size="125" class="ma-4 mb-0">
+          <v-img contain :src="image"></v-img>
+        </v-avatar>
+      </div>
+      <div class="d-flex flex-nowrap justify-space-between align-center pa-4 pt-0">
+        <div>
           <Tag name="Groupset" :value="groupset" />
           <Tag name="Brakes" :value="brakes" />
           <Tag name="Frame material" :value="frameMaterial" />
         </div>
-      </div>
-      <div class="d-flex flex-column justify-space-between">
-        <v-avatar tile size="125">
-          <v-img contain :src="image"></v-img>
-        </v-avatar>
-        <div class="text-center bike-card-on-hover">
+        <div class="text-center bike-card-on-hover" style="width: 125px;">
           <v-btn
             fab
             x-small
             depressed
-            class="ma-1"
+            class="mx-1"
             @click="editMode = !editMode"
           >
             <v-icon>
               {{ editMode ? "mdi-arrow-up-drop-circle" : "mdi-pencil" }}
             </v-icon>
           </v-btn>
-          <v-btn fab x-small depressed class="ma-1" @click="removeBike">
+          <v-btn fab x-small depressed class="mx-1" @click="removeBike">
             <v-icon> mdi-delete </v-icon>
           </v-btn>
         </div>
